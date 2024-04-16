f8e7675bba99c3a9e7ffde1468544ccff6df9d23
dapp-sculptor <hugodevpro@outlook.com>
2024-04-16 23:11:20 +0900

    feat: fetch

--- src/modules/WalletChecker.tsx
+++ src/modules/WalletChecker.tsx
@@ -1,52 +1,94 @@
 /* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
 import {
   CheckHoldingIcon,
   PasteIcon,
   PolygonIcon,
 } from "../components/SvgList";
+import axios from "axios";
 
 const WalletChecker = () => {
+  const [address, setAddress] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
+  const [bronze, setBronze] = useState<number>(0)
+  const [silver, setSilver] = useState<number>(0)
+  const [gold, setGold] = useState<number>(0)
+  const [diamond, setDiamond] = useState<number>(0)
+
+  const fetch = async () => {
+    try {
+      setLoading(true)
+      const res: any = (await axios.post('http://localhost:9000/api/users/amount', { address })).data
+      setBronze(res.bronze)
+      setSilver(res.silver)
+      setGold(res.gold)
+      setDiamond(res.diamond)
+      setLoading(false)
+    } catch (e) {
+      setBronze(0)
+      setSilver(0)
+      setGold(0)
+      setDiamond(0)
+      setLoading(false)
+      console.log(e)
+    }
+  }
+
   return (
-    <div className="w-full flex items-center justify-center flex-col relative">
-      <div className="absolute filter rounded-full -z-[9997] md:top-[150px] top-[120px] left-0 right-0 flex items-center justify-center">
+    <div className="relative flex flex-col justify-center items-center w-full">
+      {/* {loading && <div className="top-0 left-0 fixed bg-[#6d6d6d8a] backdrop-blur-md w-[100vw] h-[100vh]"></div>} */}
+      <div className="top-[120px] md:top-[150px] right-0 left-0 -z-[9997] absolute flex justify-center items-center rounded-full filter">
         <img
           src="/imgs/centerMark.png"
-          className="w-[370px] -z-[9998] h-full md:opacity-20 opacity-20 mr-[20px]"
+          className="-z-[9998] opacity-20 md:opacity-20 mr-[20px] w-[370px] h-full"
           alt="CenterMark"
         />
       </div>
-      <h1 className="md:text-[48px] text-[30px] font-extrabold text-center bg-gradient-to-r from-[#6BF58C] to-[#148F2F] bg-clip-text text-transparent md:leading-[65px] leading-[50px]">
+      <h1 className="bg-clip-text bg-gradient-to-r from-[#6BF58C] to-[#148F2F] font-extrabold text-[30px] text-center text-transparent md:text-[48px] leading-[50px] md:leading-[65px]">
         {`CHECK YOUR $TEK`} <br />
         {` HOLDINGS`}
       </h1>
-      <div className="md:w-[626px] w-full flex gap-[14px] flex-col mt-[200px]">
-        <div className="bg-black bg-opacity-40 rounded-[24px] md:p-8 p-6 flex gap-[14px] flex-col w-full">
-          <div className="gap-2 flex items-center justify-start">
+      <div className="flex flex-col gap-[14px] mt-[200px] w-full md:w-[626px]">
+        <div className="flex flex-col gap-[14px] bg-black bg-opacity-40 p-6 md:p-8 rounded-[24px] w-full">
+          <div className="flex justify-start items-center gap-2">
             <PasteIcon />
-            <p className="uppercase md:text-[16px] text-[14px] text-white text-opacity-40 font-bold">{`paste your wallet address`}</p>
+            <p className="font-bold text-[14px] text-white md:text-[16px] text-opacity-40 uppercase">{`paste your wallet address`}</p>
           </div>
-          <div className="w-full bg-white bg-opacity-10 rounded-md flex items-center justify-between md:p-5 p-3">
+          <div className="flex justify-between items-center bg-white bg-opacity-10 p-3 md:p-5 rounded-md w-full">
             <input
-              className="h-[28px] md:text-[16px] text-[14px] w-[90%] bg-transparent placeholder:font-bold placeholder:uppercase placeholder:md:text-[16px] placeholder:text-[14px] outline-none text-white placeholder:text-[#7A787A] font-bold"
+              className="bg-transparent w-[90%] h-[28px] font-bold placeholder:font-bold text-[14px] text-white md:text-[16px] placeholder:md:text-[16px] placeholder:text-[#7A787A] placeholder:text-[14px] placeholder:uppercase outline-none"
               placeholder="Please drop your address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
             />
             <PolygonIcon color={""} />
           </div>
-          <button className="w-full p-[13px] rounded-md bg-gradient-to-tr from-[#6BF58C] to-[#148F2F] flex items-center justify-center gap-4 shadow-btn">
+          <button className="flex justify-center items-center gap-4 bg-gradient-to-tr from-[#6BF58C] hover:from-[#6bf58b8a] to-[#148F2F] hover:to-[#148f2f85] shadow-btn p-[13px] rounded-md w-full">
             <p
-              className="md:text-[16px] text-[14px] font-bold text-white uppercase"
+              className="font-bold text-[14px] text-white md:text-[16px] uppercase"
               style={{
                 textShadow: "0 1px 0px rgba(0, 25, 66, 0.4)",
               }}
+              onClick={() => fetch()}
             >{`CHECK YOUR $TEK HOLDINGS`}</p>
             <CheckHoldingIcon />
           </button>
+          <div className="flex justify-between text-white">
+            <div className="flex justify-center items-center gap-3"><img src="/imgs/bronze.png" className="w-10 h-10 object-cover" alt="mark"/>
+            <p className="font-bold text-white text-xl">{bronze}</p></div>
+            <div className="flex justify-center items-center gap-3"><img src="/imgs/silver.png" className="w-10 h-10 object-cover" alt="mark"/>
+            <p className="font-bold text-white text-xl">{silver}</p></div>
+            <div className="flex justify-center items-center gap-3"><img src="/imgs/gold.png" className="w-10 h-10 object-cover" alt="mark"/>
+            <p className="font-bold text-white text-xl">{gold}</p></div>
+            <div className="flex justify-center items-center gap-3"><img src="/imgs/diamond.png" className="w-10 h-10 object-cover" alt="mark"/>
+            <p className="font-bold text-white text-xl">{diamond}</p></div>
+          </div>
         </div>
-        <div className="w-full flex items-center justify-center gap-6 rounded-3xl bg-white bg-opacity-10 p-[24px]">
-          <p className="text-white md:text-[16px] text-[14px] uppercase font-bold">{`BUY TEKIO & START MINING`}</p>
+        <div className="flex justify-center items-center gap-6 bg-white bg-opacity-10 p-[24px] rounded-3xl w-full">
+          <p className="font-bold text-[14px] text-white md:text-[16px] uppercase">{`BUY TEKIO & START MINING`}</p>
           <img
             src="/imgs/dewIcon.png"
-            className="md:w-[40px] md:h-[40px] w-[20px] h-[20px]"
+            className="w-[20px] md:w-[40px] h-[20px] md:h-[40px]"
             alt="Dew Icon"
           />
         </div>
